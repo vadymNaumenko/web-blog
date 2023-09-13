ea655c1d6a3fdece761172d980a6b2b3ffc024fe
VdymHunter <vadimnaumnaum@gmail.com>
2023-09-13 14:10:50 +0200

    created new contreller WidgetsController and add Get methd wetter()

---
+++ src/main/java/com/naumenco/blog/controlers/WidgetsController.java
@@ -0,0 +1,17 @@
+package com.naumenco.blog.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/widgets")
+public class WidgetsController {
+
+    @GetMapping("/wetter")
+    public String wetter(){
+        return "wetter";
+    }
+
+}
